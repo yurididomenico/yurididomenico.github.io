e13634db8572796be2aa1502c1f28b205a311eed
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2023-04-27 19:28:58 +0200

    Mostra Pagine senza refresh

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,53 +1,67 @@
 "use strict";
 
+// const linkPage = document.querySelectorAll(".linkPage");
+// const homeLink = document.getElementById("home");
+// const homeDiv = document.querySelector(".home");
+
+// const aboutLink = document.getElementById("about");
+// const aboutDiv = document.querySelector(".about");
+
+// const esperienzaLink = document.getElementById("esperienza");
+// const esperienzaDiv = document.querySelector(".esperienza");
+
+// const portfolioLink = document.getElementById("portfolio");
+// const portfolioDiv = document.querySelector(".portfolio");
+
+// const contattiLink = document.getElementById("contatti");
+// const contattiDiv = document.querySelector(".contatti");
+
+// homeLink.addEventListener("click", function () {
+//   console.log(home);
+//   linkPage.forEach((element) => {
+//     element.classList.add("hidden");
+//   });
+//   homeDiv.classList.remove("hidden");
+// });
+
+// aboutLink.addEventListener("click", function () {
+//   linkPage.forEach((element) => {
+//     element.classList.add("hidden");
+//   });
+//   aboutDiv.classList.remove("hidden");
+// });
+
+// esperienzaLink.addEventListener("click", function () {
+//   linkPage.forEach((element) => {
+//     element.classList.add("hidden");
+//   });
+//   esperienzaDiv.classList.remove("hidden");
+// });
+
+// portfolioLink.addEventListener("click", function () {
+//   linkPage.forEach((element) => {
+//     element.classList.add("hidden");
+//   });
+//   portfolioDiv.classList.remove("hidden");
+// });
+
+// contattiLink.addEventListener("click", function () {
+//   linkPage.forEach((element) => {
+//     element.classList.add("hidden");
+//   });
+//   contattiDiv.classList.remove("hidden");
+// });
+
+const linkHead = document.querySelectorAll(".linkHead");
 const linkPage = document.querySelectorAll(".linkPage");
-const homeLink = document.getElementById("home");
-const homeDiv = document.querySelector(".home");
 
-const aboutLink = document.getElementById("about");
-const aboutDiv = document.querySelector(".about");
-
-const esperienzaLink = document.getElementById("esperienza");
-const esperienzaDiv = document.querySelector(".esperienza");
-
-const portfolioLink = document.getElementById("portfolio");
-const portfolioDiv = document.querySelector(".portfolio");
-
-const contattiLink = document.getElementById("contatti");
-const contattiDiv = document.querySelector(".contatti");
-
-homeLink.addEventListener("click", function () {
-  console.log(home);
-  linkPage.forEach((element) => {
-    element.classList.add("hidden");
+linkHead.forEach((element) => {
+  element.addEventListener("click", function () {
+    linkPage.forEach((el) => {
+      el.classList.add("hidden");
+      if (element.id == el.classList[0]) {
+        el.classList.remove("hidden");
+      }
+    });
   });
-  homeDiv.classList.remove("hidden");
-});
-
-aboutLink.addEventListener("click", function () {
-  linkPage.forEach((element) => {
-    element.classList.add("hidden");
-  });
-  aboutDiv.classList.remove("hidden");
-});
-
-esperienzaLink.addEventListener("click", function () {
-  linkPage.forEach((element) => {
-    element.classList.add("hidden");
-  });
-  esperienzaDiv.classList.remove("hidden");
-});
-
-portfolioLink.addEventListener("click", function () {
-  linkPage.forEach((element) => {
-    element.classList.add("hidden");
-  });
-  portfolioDiv.classList.remove("hidden");
-});
-
-contattiLink.addEventListener("click", function () {
-  linkPage.forEach((element) => {
-    element.classList.add("hidden");
-  });
-  contattiDiv.classList.remove("hidden");
 });
