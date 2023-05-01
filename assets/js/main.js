01fe8cc9e6cf8c49713ce126f61db89fadb20e3d
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2023-05-01 16:23:38 +0200

    Fix bug hambMenu

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,29 @@
 const linkHead = document.querySelectorAll(".linkHead");
 const linkPage = document.querySelectorAll(".linkPage");
 
+// HambMenu
+const hambMenu = document.getElementById("hambMenu");
+const hambTendina = document.getElementById("tendinaLinks");
+const selectOverlay = document.querySelector(".overlay");
+let toggleMenu = false;
+
+function rotateMenu() {
+  if (toggleMenu) {
+    hambMenu.style.transform = "rotate(0deg)";
+  } else {
+    hambMenu.style.transform = "rotate(90deg)";
+  }
+  toggleMenu = !toggleMenu;
+}
+
 linkHead.forEach((element) => {
   element.addEventListener("click", function () {
     // Nasconde tendina (mobile) e overlay sfocato
     hambTendina.classList.add("hidden");
     selectOverlay.classList.add("hidden");
+
+    //Reset Hamburger
+    rotateMenu();
 
     // Elimina da tutti i link la pagina attiva (css)
     linkHead.forEach((x) => {
@@ -28,17 +46,15 @@ linkHead.forEach((element) => {
 });
 
 // Hamburger menu
-const hambMenu = document.getElementById("hambMenu");
-const hambTendina = document.getElementById("tendinaLinks");
-const selectOverlay = document.querySelector(".overlay");
-
 hambMenu.addEventListener("click", function () {
+  rotateMenu();
   hambTendina.classList.toggle("fade");
   hambTendina.classList.toggle("hidden");
   selectOverlay.classList.toggle("hidden");
 });
 
 selectOverlay.addEventListener("click", function () {
+  rotateMenu();
   hambTendina.classList.add("hidden");
   selectOverlay.classList.add("hidden");
 });
